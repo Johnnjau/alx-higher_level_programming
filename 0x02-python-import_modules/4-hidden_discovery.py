df76026d9ea5431d3ca04e9f87f00e173e9e6316
Johnnjau <Johnnjau202@gmail.com>
2023-03-09 19:36:35 -0800

    102-magic_calculation.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,9 +2,9 @@
 
 if __name__ == "__main__":
     """Print all names defined by hidden_4 module."""
-    from hidden_4.py import hidden_4.pyc
+    from hidden_4.pyc import name
 
-    names = dir(hidden_4)
+    names = dir(hidden_4.pyc)
     for names in names:
         if name[:2] != "__":
             print(name)
