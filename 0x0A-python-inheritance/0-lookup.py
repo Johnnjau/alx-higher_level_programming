55d6003e13306997b9e6336dff4b6719ee291b57
Johnnjau <Johnnjau202@gmail.com>
2023-04-11 16:01:38 -0700

    0-lookup.py

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""Defines an object attribute lookup function"""
+
+def lookup(obj_to_inspect):
+    """Return a list of an object's attributes."""
+    return dir(obj_to_inspect)
