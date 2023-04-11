7c77a5bccf44d2975d7f8ec6ed753a344a6291cd
Johnnjau <Johnnjau202@gmail.com>
2023-04-11 16:37:42 -0700

    1-my_list.py

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+class MyList(list):
+    """Implements sorted printing for the built-in list class"""
+
+    def print_sorted(self):
+        """Print the list in ascending order."""
+        sorted_list = sorted(self)
+        print(f"The sorted list is: {sorted_list}")
