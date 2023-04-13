a3827f52dbec8ce65ba5d7f25a26c22c629cdfb3
Johnnjau <Johnnjau202@gmail.com>
2023-04-13 14:19:45 -0700

    3-main.py

--- 0x0A-python-inheritance/3-main.py
+++ 0x0A-python-inheritance/3-main.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-from 3-is_kind_of_class.py import is_instance_or_inherited
+is_kind_of_class = __import__('3-is_kind_of_class').is_kind_of_class
 
 a = 1
-if is_instance_or_inherited(a, int):
+if is_kind_of_class(a, int):
     print("{} comes from {}".format(a, int.__name__))
 if is_kind_of_class(a, float):
     print("{} comes from {}".format(a, float.__name__))
