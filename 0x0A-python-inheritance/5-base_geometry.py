76ddab9b4de2fa5100d77439759ad3ad37b37e3b
Johnnjau <Johnnjau202@gmail.com>
2023-04-13 17:52:41 -0700

    5-base_geometry.py

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This module defines the BaseGeometry class."""
+
+
+class BaseGeometry:
+    """A class that represents base geometry."""
+    def __init__(self):
+        """Initializes a new instance of the BaseGeometry class."""
+        pass
