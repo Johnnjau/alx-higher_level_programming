19fa4205c684f63eff15d07fe03bb961140d3844
Johnnjau <Johnnjau202@gmail.com>
2023-04-15 13:17:26 -0700

    100-my_int.py

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""This module defines a class MyInt that inherits from int
+and inverts the == and != operators."""
+
+
+class MyInt(int):
+    """A class that inherits from int and inverts the == and != operators."""
+
+
+    def __eq__(self, value):
+        """Override the == operator with the behavior of !=."""
+        return self.real != value
+
+    def __ne__(self, value):
+        """Override the != operator with the behavior of ==."""
+        return self.real == value
