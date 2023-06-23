dd1cec315c07e7e6a8eb723a9397eb45dc6d9fd9
Johnnjau <Johnnjau202@gmail.com>
2023-06-23 06:23:48 -0700

    1-filter_states.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -3,11 +3,25 @@
 # Usage: ./1-filter_states.py <mysql username> \
 #                             <mysql password> \
 #                             <database name>
-import sys
 import MySQLdb
+import sys
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
-    c.execute("SELECT * FROM `states` ORDER BY `id`")
-    [print(state) for state in c.fetchall() if state[1][0] == "N"]
+# Establish the database connection
+db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
+
+# Create a cursor object
+c = db.cursor()
+
+# Execute the SQL query
+c.execute("SELECT * FROM `states`")
+
+# Fetch the results
+results = c.fetchall()
+
+# Process the results (example: print the data)
+for row in results:
+    print(row)
+
+# Close the cursor and database connection
+c.close()
+db.close()
