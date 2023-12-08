c99d2c489ff95f8b804e53d1fd225937d0ac6d47
Johnnjau <72441100+Johnnjau@users.noreply.github.com>
2023-12-08 20:03:22 +0300

    Delete 0x0A-python-inheritance directory

--- 0x0A-python-inheritance/0-lookup.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-
-"""Defines an object attribute lookup function"""
-
-
-def lookup(obj_to_inspect):
-    """Return a list of an object's attributes."""
-    return dir(obj_to_inspect)
