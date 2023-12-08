e3eb0ee5c3188eb5e40815790b0bf2cf2f7aa89e
Johnnjau <72441100+Johnnjau@users.noreply.github.com>
2023-12-08 20:02:35 +0300

    Delete 0x0E-SQL_more_queries directory

--- 0x0E-SQL_more_queries/3-force_name.sql
+++
@@ -1,5 +0,0 @@
--- Creation of the table force_name.
-CREATE TABLE IF NOT EXISTS `force_name` (
-	`id`   INT,
-	`name` VARCHAR(256) NOT NULL
-);
--- 0x0E-SQL_more_queries/5-unique_id.sql
+++
@@ -1,5 +0,0 @@
--- creation of table unique_id.
-CREATE TABLE `unique_id` (
-    `id` INT DEFAULT 1 UNIQUE,
-    `name` VARCHAR(256)
-);
