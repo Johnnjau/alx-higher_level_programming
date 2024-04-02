6c89f86283d1bd0adaee9842231ce9d46117b4d5
John <johnnjau202@gmail.com>
2024-04-02 18:42:29 +0300

    3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -6,14 +6,14 @@ const episodeNumber = process.argv[2];
 
 // Make a request to the Star Wars API
 request(`https://swapi-api.alx-tools.com/api/films/${episodeNumber}`, (error, response, body) => {
-    if (error) {
-        console.error('Error:', error);
-    } else if (response.statusCode !== 200) {
-        console.error('Unexpected status code:', response.statusCode);
-    } else {
-        // Parse the JSON response
-        const film = JSON.parse(body);
-        // Print the title of the movie
-        console.log(film.title);
-    }
+  if (error) {
+    console.error('Error:', error);
+  } else if (response.statusCode !== 200) {
+    console.error('Unexpected status code:', response.statusCode);
+  } else {
+    // Parse the JSON response
+    const film = JSON.parse(body);
+    // Print the title of the movie
+    console.log(film.title);
+  }
 });
