7935d537cce09c6e157d8d578783a990858b59b4
John <johnnjau202@gmail.com>
2024-04-02 18:41:05 +0300

    3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,6 +1,19 @@
 #!/usr/bin/node
 const request = require('request');
-const url = 'http://swapi.co/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
-  console.log(error || JSON.parse(body).title);
+
+// Get the episode number from the command line arguments
+const episodeNumber = process.argv[2];
+
+// Make a request to the Star Wars API
+request(`https://swapi-api.alx-tools.com/api/films/${episodeNumber}`, (error, response, body) => {
+    if (error) {
+        console.error('Error:', error);
+    } else if (response.statusCode !== 200) {
+        console.error('Unexpected status code:', response.statusCode);
+    } else {
+        // Parse the JSON response
+        const film = JSON.parse(body);
+        // Print the title of the movie
+        console.log(film.title);
+    }
 });
